ec85ad2df693e81f7ef9f01c4cf76a1745736e0c
Shusen <liushusen73@gmail.com>
2023-11-25 21:49:04 +0800

    Update nvim-cmp keymappings
    - <C-n> trigger cmp.mapping.complete{} by nvim-cmp default setting already.
    - <C-space> conflict with MacOS language switching

--- init.lua
+++ init.lua
@@ -586,11 +586,11 @@ cmp.setup {
     completeopt = 'menu,menuone,noinsert'
   },
   mapping = cmp.mapping.preset.insert {
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
+    -- ['<C-n>'] = cmp.mapping.select_next_item(),
+    -- ['<C-Space>'] = cmp.mapping.complete {},
+    -- ['<C-p>'] = cmp.mapping.select_prev_item(),
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete {},
     ['<CR>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
