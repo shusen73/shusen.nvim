6a02e753599630b9c85b88f6560dec420d9c64b2
Shusen <liushusen73@gmail.com>
2023-12-03 13:36:59 +0800

    Add Github Copilot

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,30 @@
+return {
+  "zbirenbaum/copilot.lua",
+  cmd = "Copilot",
+  event = "InsertEnter",
+  config = function()
+    require("copilot").setup({
+      panel = {
+      },
+      suggestion = {
+        auto_trigger = true,
+        keymap = {
+          accept = "<C-f>"
+        }
+      },
+      filetypes = {
+      }
+    })
+
+    -- hide copilot suggestions when cmp menu is open
+    -- to prevent odd behavior/garbled up suggestions
+    local cmp = require 'cmp'
+    local copilot = require 'copilot'
+    cmp.event:on("menu_opened", function()
+      vim.b.copilot_suggestion_hidden = true
+    end)
+    cmp.event:on("menu_closed", function()
+      vim.b.copilot_suggestion_hidden = false
+    end)
+  end,
+}
