1fa7e7da52500d04d75e68dd8c81b0c2aa2bab34
Shusen <liushusen73@gmail.com>
2024-01-01 21:36:21 +0800

    Revert harpoon keymapping

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -16,7 +16,7 @@ return {
 
     vim.keymap.set("n", "<C-f>", function() harpoon:list():select(1) end, { desc = 'Harpoon: select 1' })
     vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end, { desc = 'Harpoon: select 2' })
-    vim.keymap.set("n", "<C-d>", function() harpoon:list():select(3) end, { desc = 'Harpoon: select 3' })
-    vim.keymap.set("n", "<C-k>", function() harpoon:list():select(4) end, { desc = 'Harpoon: select 4' })
+    vim.keymap.set("n", "<C-g>", function() harpoon:list():select(3) end, { desc = 'Harpoon: select 3' })
+    vim.keymap.set("n", "<C-h>", function() harpoon:list():select(4) end, { desc = 'Harpoon: select 4' })
   end,
 }
