d10ef611fff744a2727bb8ea1052b9570f92a741
Shusen <liushusen73@gmail.com>
2024-03-16 02:50:28 +0800

    Add codeium.lua to plugins

---
+++ lua/custom/plugins/codeium.lua
@@ -0,0 +1,10 @@
+return {
+  'Exafunction/codeium.vim',
+  event = 'InsertEnter',
+  config = function()
+    -- Change '<C-g>' here to any keycode you like.
+    vim.keymap.set('i', '<C-f>', function()
+      return vim.fn['codeium#Accept']()
+    end, { expr = true, silent = true })
+  end,
+}
