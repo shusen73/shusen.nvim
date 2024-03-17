f4f92baccd97ddc27751e87f947512718835106a
Shusen <19257104+shusen73@users.noreply.github.com>
2024-03-17 14:28:39 +0800

    Update

--- lua/custom/plugins/codeium.lua
+++ lua/custom/plugins/codeium.lua
@@ -3,7 +3,7 @@ return {
   event = 'InsertEnter',
   config = function()
     -- Change '<C-g>' here to any keycode you like.
-    vim.keymap.set('i', '<C-f>', function()
+    vim.keymap.set('i', '<C-g>', function()
       return vim.fn['codeium#Accept']()
     end, { expr = true, silent = true })
   end,
