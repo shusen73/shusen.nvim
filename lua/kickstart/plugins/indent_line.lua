03a79a196181e7c9af90cc394071e71ffc43a1c0
Shusen <19257104+shusen73@users.noreply.github.com>
2024-05-31 16:49:48 +0800

    Update char for indent-blankline

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -1,10 +1,16 @@
 return {
-  { -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    lazy = false,
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
+  -- Add indentation guides even on blank lines
+  'lukas-reineke/indent-blankline.nvim',
+  lazy = false,
+  -- Enable `lukas-reineke/indent-blankline.nvim`
+  -- See `:help ibl`
+  main = 'ibl',
+  opts = {},
+  config = function()
+    require('ibl').setup {
+      indent = {
+        char = '┊',
+      },
+    }
+  end,
 }
