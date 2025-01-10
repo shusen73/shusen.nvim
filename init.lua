2dfd3d9b798f656a080a1dbb1b57fdc1c92e17b2
Shusen <19257104+shusen73@users.noreply.github.com>
2025-01-10 22:25:11 +0900

    Update

--- init.lua
+++ init.lua
@@ -114,7 +114,7 @@ vim.opt.showmode = false
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
--- vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.opt.breakindent = true
